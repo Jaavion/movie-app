e3e72a823066542a11eb3c26c1d161afc01ab72a
Jaavion <jaavion86@gmail.com>
2021-05-07 05:59:36 -0700

    created a new route for my controller as well as added additional method to my controller file

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,8 +3,8 @@ class MoviesController < ApplicationController
     movies = Movie.all 
     render json: movies.to_json
   end
-  def first_movies 
-    movies = Movie.first
-    render json: movies.to_json
+  def first_movie
+    movie = Movie.first
+    render json: movie.to_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "all_movies", controller: "movies", action: "all_movies"
+  get "first_movie", controller: "movies", action: "first_movie"
 end
